6e8ce3e7a326dadc00554b350748fcb91cc624e6
Albert DeFusco <albert.defusco@me.com>
2021-12-26 17:44:18 -0600

    use anaconda-project run in entrypoint
    this is a much better idea than to activate the conda env

--- s2i/bin/entrypoint.sh
+++ s2i/bin/entrypoint.sh
@@ -1,13 +1,11 @@
 #!/bin/bash
 
-eval "$(conda shell.bash hook)"
-
 if [ -e /opt/app-root/src/.assembled ]; then
-    project_command=${CMD:-default}
-    env_spec=$(python -c "from anaconda_project.project import Project;print(Project('.').command_for_name('${project_command}').default_env_spec_name)")
-    conda activate ./envs/${env_spec}
+    if [[ $1 == "/usr/libexec/s2i/run" ]]; then
+        exec tini -g -- "$@"
+    else
+        exec tini -g -- anaconda-project run "$@"
+    fi
 else
-    conda activate base
+    exec tini -g -- "$@"
 fi
-
-exec tini -g -- "$@"
